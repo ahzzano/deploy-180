91b2079973fd8c914f7ecabed390e86d1bca44e2
W. Santos <w.santos.gg@gmail.com>
2025-05-25 21:47:34 +0800

    feat: change vite server ports to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	preview: {
+		port: 3000,
+		host: true
+	},
+	server: {
+		port: 3000,
+		host: true
+	},
 	test: {
 		workspace: [
 			{
