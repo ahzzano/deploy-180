e40b4c3789a75ba5759c22fd3eca1dbd5d7fce31
W. Santos <w.santos.gg@gmail.com>
2025-05-25 12:08:06 +0800

    feat: add bottom margin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 <!-- <div class="navbar bg-base-200 shadow-sm"></div> -->
 
 <div
-	class="mr-8 ml-8 md:mr-48 md:ml-48 xl:mr-96 xl:ml-96 mt-16 flex flex-col gap-8"
+	class="mr-8 ml-8 md:mr-48 md:ml-48 xl:mr-96 xl:ml-96 mt-16 mb-16 flex flex-col gap-8"
 >
 	{@render children()}
 </div>
