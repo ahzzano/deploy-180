8e739573af6725e638511dd9fcf8910bc819b6f2
W. Santos <w.santos.gg@gmail.com>
2025-05-25 11:34:54 +0800

    feat: add responsiveness to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,10 @@
 	let { children } = $props();
 </script>
 
-<div class="navbar bg-base-200 shadow-sm"></div>
+<!-- <div class="navbar bg-base-200 shadow-sm"></div> -->
 
-<div class="pr-96 pl-96 mt-16 flex flex-col gap-8">
+<div
+	class="mr-8 ml-8 md:mr-48 md:ml-48 xl:mr-96 xl:ml-96 mt-16 flex flex-col gap-8"
+>
 	{@render children()}
 </div>
