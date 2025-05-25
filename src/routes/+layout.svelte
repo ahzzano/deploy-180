3afe20ee5c9f6d270367477461c910bf7f04b44c
W. Santos <w.santos.gg@gmail.com>
2025-05-25 09:36:13 +0800

    feat: add significant layout changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import '../app.css';
-	
+	import "../app.css";
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="navbar bg-base-200 shadow-sm"></div>
+
+<div class="pr-96 pl-96 mt-16 flex flex-col gap-8">
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,48 @@
 <script lang="ts"></script>
 
-<div class="nav"></div>
+<div class="card bg-base-200 shadow-sm">
+    <div class="card-body">
+        <span class="card-title">Banana Leaf Classifier</span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, nulla
+        est. Quam, distinctio corrupti sed saepe explicabo molestias totam minus
+        alias hic eligendi modi provident atque reprehenderit, magni dolores pariatur!
+    </div>
+</div>
+
+<div class="card bg-base-200 shadow-sm">
+    <div class="card-body">
+        <span class="card-title">About</span>
+        lorem ipsum dolor sit amet consectetur adipisicing elit. voluptatum, nulla
+        est. quam, distinctio corrupti sed saepe explicabo molestias totam minus
+        alias hic eligendi modi provident atque reprehenderit, magni dolores pariatur!
+    </div>
+</div>
+
+<div class="card bg-base-200 shadow-sm">
+    <div class="card-body">
+        <span class="card-title">Models</span>
+        <p>Here are the models presented in our presentation</p>
+        <div class="join join-vertical">
+            <div
+                class="collapse collapse-arrow join-item border-base-300 border"
+            >
+                <input type="radio" name="my-accordion-4" checked={true} />
+                <div class="collapse-title font-semibold">EfficientNet</div>
+                <div class="collapse-content text-sm">
+                    Click the "Sign Up" button in the top right corner and
+                    follow the registration process.
+                </div>
+            </div>
+            <div
+                class="collapse collapse-arrow join-item border-base-300 border"
+            >
+                <input type="radio" name="my-accordion-4" />
+                <div class="collapse-title font-semibold">VGG19 + kNN</div>
+                <div class="collapse-content text-sm">
+                    Click the "Sign Up" button in the top right corner and
+                    follow the registration process.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
